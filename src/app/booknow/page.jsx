6194621f8f2ec2771bac767e21af56c211883716
Rohit005005 <137618172+Rohit005005@users.noreply.github.com>
2025-03-24 00:54:45 +0530

    book now heading size

--- src/app/booknow/page.jsx
+++ src/app/booknow/page.jsx
@@ -5,8 +5,10 @@ import Rooms from './_components/Rooms'
 const page = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10 font-light">Book Now</h1>
-      <hr className="w-1/4 mx-auto my-5" />
+      <h1 className="sm:text-5xl text-2xl text-center mt-7 sm:mt-10 font-light">
+        Book Now
+      </h1>
+      <hr className="w-2/4 mx-auto sm:my-10 my-3" />
       <div className='flex flex-col sm:flex-row'>
         <Options/>
         <Rooms/>
